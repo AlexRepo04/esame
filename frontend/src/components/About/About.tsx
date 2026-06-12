62b066605b2f8e9b1fbedf3c756fe0a4f0b9c620
root <alexbra.info@gmail.com>
2026-06-12 09:18:27 +0200

    fix style

---
+++ frontend/src/components/About/About.tsx
@@ -0,0 +1,18 @@
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about-section">
+      <h2 className="about-title">Chi siamo</h2>
+
+      <p className="about-desc">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quos
+        suscipit error. Provident sit, voluptate a repellendus incidunt
+        accusantium quam fugiat, eius ratione neque maxime! Neque soluta
+        deleniti dolorum a?
+      </p>
+    </div>
+  );
+}
+
+export default About;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,19 @@
-import Hero from '../components/Hero/Hero'
-import Cards from '../components/Cards/Cards'
-import ContactForm from '../components/Form/Form'
-import FeaturedProducts from '../components/Products/FeaturedProducts'
+import Hero from "../components/Hero/Hero";
+import Cards from "../components/Cards/Cards";
+import ContactForm from "../components/Form/Form";
+import FeaturedProducts from "../components/Products/FeaturedProducts";
+import About from "../components/About/About";
 
 function Home() {
   return (
     <div>
       <Hero />
+      <About></About>
       <Cards />
-      <FeaturedProducts limit={3} /> 
+      <FeaturedProducts limit={3} />
       <ContactForm />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
